c8ba98b46bbda4c5712a79cf3961779ff4352c2e
Faiyaz Murshed <murshed.faiyaz@gmail.com>
2025-01-29 00:18:34 +0600

    added Join link

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import '@fontsource/poppins/400.css'; // Regular
-import '@fontsource/poppins/500.css'; // Medium
-import '@fontsource/poppins/600.css'; // SemiBold
-import '@fontsource/poppins/700.css'; // Bold
+import "@fontsource/poppins/400.css"; // Regular
+import "@fontsource/poppins/500.css"; // Medium
+import "@fontsource/poppins/600.css"; // SemiBold
+import "@fontsource/poppins/700.css"; // Bold
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
+import Join from "./pages/Join/Join.tsx";
 import { ViewportProvider } from "./ViewportContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
@@ -15,8 +16,9 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
+          <Route path="/join" element={<Join />} />
         </Routes>
       </BrowserRouter>
     </ViewportProvider>
-  </StrictMode>
+  </StrictMode>,
 );
