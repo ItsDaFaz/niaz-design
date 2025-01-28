7541b6155223c8b9dd1b61274feca3a929875913
Faiyaz Murshed <murshed.faiyaz@gmail.com>
2025-01-28 17:59:52 +0600

    Updated header to show more buttons in non-mobile view

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,23 @@
-import baylaLogo from '../../assets/baylaLogo.png';
-import headerMenu from '../../assets/header-menu.png';
-import { useViewport } from '../../useViewport';
-
+import baylaLogo from "../../assets/baylaLogo.png";
+import headerMenu from "../../assets/header-menu.png";
+import { useViewport } from "../../useViewport";
+import "./Header.css";
 const Header = () => {
   const { isMobile } = useViewport();
   return (
     <header
       style={{
-        position: 'fixed',
+        position: "fixed",
         top: 0,
         left: 0,
-        width: '100%',
-        backgroundColor: 'rgb(255, 255, 255)',
-        height: isMobile?'8vh':'11vh',
-        display: 'grid', // Use CSS Grid for layout
-        gridTemplateColumns: '1fr auto 1fr', // Create three columns
-        alignItems: 'center', // Vertically align content
-        padding: '0 7.5vw', // Padding for responsiveness
-        boxSizing: 'border-box',
+        width: "100%",
+        backgroundColor: "rgb(255, 255, 255)",
+        height: isMobile ? "8vh" : "12vh",
+        display: "grid", // Use CSS Grid for layout
+        gridTemplateColumns: "1fr auto 1fr", // Create three columns
+        alignItems: "center", // Vertically align content
+        padding: "0 7.5vw", // Padding for responsiveness
+        boxSizing: "border-box",
       }}
     >
       {/* Empty div to push logo to center */}
@@ -26,21 +26,42 @@ const Header = () => {
       {/* Centered Logo */}
       <img
         src={baylaLogo}
-        style={{ height: '3.5rem', objectFit: 'contain' }}
+        style={{ height: "3.5rem", objectFit: "contain" }}
         alt="Logo"
       />
 
       {/* Menu Button on the far right */}
-      <img
-        src={headerMenu}
+      <div
         style={{
-          height: '15%',
-          objectFit: 'contain',
-          cursor: 'pointer',
-          justifySelf: 'end', // Aligns to the right edge
+          display: "flex",
+          padding: 0,
+          margin: 0,
+          justifyContent: "flex-end",
+          alignContent: "right",
         }}
-        alt="Menu"
-      />
+      >
+        {isMobile ? (
+          <img
+            src={headerMenu}
+            style={{
+              height: "15%",
+              objectFit: "contain",
+              cursor: "pointer",
+              justifySelf: "end", // Aligns to the right edge
+            }}
+            alt="Menu"
+          />
+        ) : (
+          <div>
+            <span id="header-right-items">Board Members</span>
+            <span id="header-right-items">Blogs</span>
+            <span id="header-right-items">Contact Us</span>
+            <span id="header-right-items">
+              <button>Join BAYLA</button>
+            </span>
+          </div>
+        )}
+      </div>
     </header>
   );
 };
