dbde3a92ce2fedee28a1cabd1de488c0c21cb8ff
Faiyaz Murshed <murshed.faiyaz@gmail.com>
2025-01-28 18:02:35 +0600

    new pages directory to store new pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,28 @@
+import "../App.css";
+import Banner1 from "../components/Banner1/Banner1";
+import Header from "../components/Header/Header";
+import ObjectiveGrid from "../components/ObjectiveGrid/ObjectiveGrid";
+import FeatureCarousel from "../components/FeatureCarousel/FeatureCarousel";
+import FooterBlock from "../components/FooterBlock/FooterBlock";
+import { useViewport } from "../useViewport";
+function Home() {
+  const { isMobile } = useViewport();
+  console.log("isMobile", isMobile);
+  return (
+    <div className="min-h-screen min-w-full flex flex-col">
+      <Header />
+      {/* <h1 className="font-poppins text-xl font-bold">Hello, Tailwind!</h1> */}
+      <Banner1 />
+      <ObjectiveGrid />
+
+      <FeatureCarousel />
+
+      {/* temporary empty div for spacing */}
+      <div style={{ height: "20vh" }}></div>
+
+      <FooterBlock />
+    </div>
+  );
+}
+
+export default Home;
